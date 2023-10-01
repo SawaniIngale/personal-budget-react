7335043e377810ae3daaf768973fe91bb8999d43
SawaniIngale <singale@uncc.edu>
2023-10-01 14:48:35 -0400

    First commit after completing the video

---
+++ src/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <div className="bottom">
+            <div className="center">
+                All rights reserved &copy; Sawani Ingale
+            </div>
+        </div>
+  );
+}
+
+export default Footer;
